ed7180753330e182fc0f1945eb254cc34319a422
isado <isadorabellaguarda@gmail.com>
2024-04-13 19:52:04 -0300

    feat: criando controller para customer

---
+++ src/main/java/br/faccat/padarqsis/customerregistry/controller/CustomerController.java
@@ -0,0 +1,44 @@
+package br.faccat.padarqsis.customerregistry.controller;
+
+import br.faccat.padarqsis.customerregistry.model.CustomerModel;
+import br.faccat.padarqsis.customerregistry.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/create")
+    public CustomerModel createCustomer(@Valid @RequestBody CustomerModel customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @GetMapping("/list")
+    public List<CustomerModel> listCustomers() {
+        return customerService.listCustomers();
+    }
+
+    @GetMapping("/get/{id}")
+    public CustomerModel getCustomerById(@RequestParam String id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public CustomerModel updateCustomer(@RequestParam String id, @Valid @RequestBody CustomerModel customer) {
+        return customerService.updateCustomer(id, customer);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteCustomer(@RequestParam String id) {
+        customerService.deleteCustomer(id);
+    }
+}
