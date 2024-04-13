60e38310453f47b1f81ddc0a56c194cdffa0c6a1
isado <isadorabellaguarda@gmail.com>
2024-04-13 18:03:33 -0300

    first commit

---
+++ src/test/java/br/faccat/padarqsis/customerregistry/CustomerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faccat.padarqsis.customerregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
