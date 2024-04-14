9012ac84d52c6cd37ce4ef41901aead56b82ab04
isado <isadorabellaguarda@gmail.com>
2024-04-14 18:45:53 -0300

    feat: adicionando serviço de mensageria ao projeto

--- src/main/java/br/faccat/padarqsis/customerregistry/CustomerRegistryApplication.java
+++ src/main/java/br/faccat/padarqsis/customerregistry/CustomerRegistryApplication.java
@@ -1,7 +1,10 @@
 package br.faccat.padarqsis.customerregistry;
 
+import br.faccat.padarqsis.customerregistry.message.MessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerRegistryApplication {
@@ -10,4 +13,11 @@ public class CustomerRegistryApplication {
         SpringApplication.run(CustomerRegistryApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner demo1(MessageProducer messageProducer) {
+        return args -> {
+            messageProducer.sendMessageToQueue1("Olá da Fila 1!");
+            messageProducer.sendMessageToQueue2("Saudações da Fila 2!");
+        };
+    }
 }
---
+++ src/main/java/br/faccat/padarqsis/customerregistry/message/MessageProducer.java
@@ -0,0 +1,25 @@
+package br.faccat.padarqsis.customerregistry.message;
+
+import br.faccat.padarqsis.customerregistry.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessageToQueue1(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.DIRECT_EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_1, message);
+        System.out.println("Mensagem enviada para a Fila 1: " + message);
+    }
+
+    public void sendMessageToQueue2(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.DIRECT_EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_2, message);
+        System.out.println("Mensagem enviada para a Fila 2: " + message);
+    }
+}
