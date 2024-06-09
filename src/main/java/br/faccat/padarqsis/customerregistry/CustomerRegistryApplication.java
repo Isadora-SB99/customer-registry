479c0189241f2e75d92f074ddc7a9f0ab4858005
isado <isadorabellaguarda@gmail.com>
2024-06-09 14:18:05 -0300

    comentando código relacionado ao rabbitMQ

--- src/main/java/br/faccat/padarqsis/customerregistry/CustomerRegistryApplication.java
+++ src/main/java/br/faccat/padarqsis/customerregistry/CustomerRegistryApplication.java
@@ -1,10 +1,7 @@
 package br.faccat.padarqsis.customerregistry;
 
-import br.faccat.padarqsis.customerregistry.message.MessageProducer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerRegistryApplication {
@@ -13,11 +10,11 @@ public class CustomerRegistryApplication {
         SpringApplication.run(CustomerRegistryApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner demo1(MessageProducer messageProducer) {
-        return args -> {
-            messageProducer.sendMessageToQueue1("Olá da Fila 1!");
-            messageProducer.sendMessageToQueue2("Saudações da Fila 2!");
-        };
-    }
+//    @Bean
+//    CommandLineRunner demo1(MessageProducer messageProducer) {
+//        return args -> {
+//            messageProducer.sendMessageToQueue1("Olá da Fila 1!");
+//            messageProducer.sendMessageToQueue2("Saudações da Fila 2!");
+//        };
+//    }
 }
--- src/main/java/br/faccat/padarqsis/customerregistry/message/MessageConsumer.java
+++ src/main/java/br/faccat/padarqsis/customerregistry/message/MessageConsumer.java
@@ -1,19 +1,19 @@
-package br.faccat.padarqsis.customerregistry.message;
-
-import br.faccat.padarqsis.customerregistry.config.RabbitMQConfig;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MessageConsumer {
-
-    @RabbitListener(queues = RabbitMQConfig.QUEUE_1_NAME)
-    public void receiveFromQueue1(String message) {
-        System.out.println("Recebido da Fila 1: " + message);
-    }
-
-    @RabbitListener(queues = RabbitMQConfig.QUEUE_2_NAME)
-    public void receiveFromQueue2(String message) {
-        System.out.println("Recebido da Fila 2: " + message);
-    }
-}
+//package br.faccat.padarqsis.customerregistry.message;
+//
+//import br.faccat.padarqsis.customerregistry.config.RabbitMQConfig;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class MessageConsumer {
+//
+//    @RabbitListener(queues = RabbitMQConfig.QUEUE_1_NAME)
+//    public void receiveFromQueue1(String message) {
+//        System.out.println("Recebido da Fila 1: " + message);
+//    }
+//
+//    @RabbitListener(queues = RabbitMQConfig.QUEUE_2_NAME)
+//    public void receiveFromQueue2(String message) {
+//        System.out.println("Recebido da Fila 2: " + message);
+//    }
+//}
