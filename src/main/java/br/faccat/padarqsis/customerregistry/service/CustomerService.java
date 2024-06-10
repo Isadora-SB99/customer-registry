26596884079e7d0f8b1e4266d1595005f256e8a2
Isadora Strottmann Bellaguarda <113373440+Isadora-SB99@users.noreply.github.com>
2024-06-09 21:57:07 -0300

    Integrating through restful (#1)
    * removing rabbitMQ
    
    * build: adding lombok dependency
    
    * refactor: applying lombok

--- src/main/java/br/faccat/padarqsis/customerregistry/service/CustomerService.java
+++ src/main/java/br/faccat/padarqsis/customerregistry/service/CustomerService.java
@@ -2,18 +2,16 @@ package br.faccat.padarqsis.customerregistry.service;
 
 import br.faccat.padarqsis.customerregistry.model.CustomerModel;
 import br.faccat.padarqsis.customerregistry.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
 
     public CustomerModel createCustomer(CustomerModel customer) {
         return customerRepository.save(customer);
